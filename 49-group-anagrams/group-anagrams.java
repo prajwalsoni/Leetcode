9489260d610c00e3d2bfa32d599b78fe4fb0c856
Prajwal Soni <143695149+prajwalsoni@users.noreply.github.com>
2024-02-06 12:25:00 +0530

    Time: 9 ms (34.75%) | Memory: 48 MB (23.33%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,31 +1,32 @@
+
+
 class Solution {
-  public List<List<String>> groupAnagrams(String[] strs) {
-
-        HashMap<String,List<String>> map=new HashMap<>();
-
-        List<List<String>> result = new ArrayList<>();
-
-
-        for (String word: strs
-        ) {
-            char[] charArray = word.toCharArray();
-
-
-            Arrays.sort(charArray);
-
-            String sortedWord = String.valueOf(charArray);
-
-            if (!map.containsKey(sortedWord)) {
-                map.put(sortedWord, new ArrayList<>());
-            }
-
-
-            map.get(sortedWord).add(word);
-
-
+    public String getSignature(String s) {
+        int[] count = new int[26];
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
         }
-        result.addAll(map.values());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
+                sb.append((char) ('a' + i)).append(count[i]);
+            }
+        }
+        return sb.toString();
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        Map<String, List<String>> groups = new HashMap<>();
+
+        for (String s : strs) {
+            groups.computeIfAbsent(getSignature(s), k -> new ArrayList<>()).add(s);
+        }
+
+        result.addAll(groups.values());
 
         return result;
     }
 }
+
