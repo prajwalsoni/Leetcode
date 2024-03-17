ac06e5d8fed9cad032cb6aae62db7ec2aae0edc8
Prajwal Soni <143695149+prajwalsoni@users.noreply.github.com>
2024-03-17 11:28:50 +0530

    Time: 1 ms (98.87%) | Memory: 45.2 MB (5.45%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,77 +1,36 @@
+
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        if(intervals.length == 0) return new int[][]{newInterval};  // is Empty return new Interval
-        int first  = newInterval[0];
-        int second = newInterval[1];
-        boolean isFind = false;     //If we find interval that is rigth, this will be true
-        int n = intervals.length;
-        List<int[]> list = new ArrayList<>();
-        for(int a = 0; a < n; a++){
-            if(isFind){ list.add(intervals[a]); continue;}  // if we find that we don't need search
-            int subF = intervals[a][0];
-            int subS = intervals[a][1];
-
-            // if first val is in array
-            if(first >= subF && first <= subS){ 
-                int[] priory = new int[2];
-                priory[0] = subF;   //setting interval's first val is Array's first val
-
-                for(int b = a; b < n; b++){// we looking for setting interval's last value
-                    if(second >= intervals[b][0]){
-                        if(intervals[b][1] >= second){// if second val is in
-                            priory[1] = intervals[b][1]; list.add(priory);
-                            a = b; isFind = true; break;
-                        }
-                    }else{// if second val isn't in
-                        priory[1] = second; list.add(priory); 
-                        a = b-1; isFind = true; break;
-                    }
-                    if(b == n-1){// if second val is most biggest value
-                        priory[1] = second; list.add(priory);
-                        a = b; isFind = true; break;
-                    }
-                }
+        List<int[]> ans = new ArrayList<>();
+        int start = newInterval[0], end = newInterval[1];
+        boolean inserted = false;
+        
+        for (int[] inv : intervals) {
+            int cstart = inv[0], cend = inv[1];
+            
+            if (cend < start || inserted) {
+                ans.add(new int[]{cstart, cend});
+                continue;
             }
-            // ----------------------------------
-            // if first val smaller than first array
-            else if(first < subF){
-                int[] priory = new int[2];
-                priory[0] = first; //setting interval's first val is our first val
-                
-                if(second < subF){// if whole interval isn't in first array
-                    priory[1] = second; list.add(priory);
-                    a --; isFind = true; continue;
-                }
-                else{
-                    for(int b = a; b < n; b++){// we looking for setting interval's last value
-                        if(second >= intervals[b][0]){
-                            if(second <= intervals[b][1]){// if second val is in
-                                priory[1] = intervals[b][1];
-                                list.add(priory); isFind = true; a = b; break; 
-                            }
-                        }
-                        else{
-                            a = b-1; priory[1] = second;
-                            list.add(priory); isFind = true; break;
-                        }
-                        if(b == n-1 && !isFind){
-                            priory[1] = second; list.add(priory);
-                            a = b; isFind = true; break;
-                        }
-                    }
-                }
+            
+            start = Math.min(start, cstart);
+            if (end < cstart) {
+                ans.add(new int[]{start, end});
+                ans.add(new int[]{cstart, cend});
+                inserted = true;
+                continue;
             }
-            // ----------------------------------
-            else{
-                list.add(intervals[a]);
+            
+            if (end <= cend) {
+                ans.add(new int[]{start, cend});
+                inserted = true;
             }
-            if(a == n-1 && !isFind) list.add(newInterval);
         }
-        int l = list.size();
-        int[][] ret = new int[l][2];
-        for(int a = 0; a < l; a++){
-            ret[a] = list.get(a);
+        
+        if (!inserted) {
+            ans.add(new int[]{start, end});
         }
-        return ret;
+        
+        return ans.toArray(new int[ans.size()][]);
     }
 }
