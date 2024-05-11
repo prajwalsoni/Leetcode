1592bfe7328d3976acf417ff1bd56ede3f956f38
Prajwal Soni <143695149+prajwalsoni@users.noreply.github.com>
2024-05-11 18:02:56 +0530

    Time: 6 ms (97.48%) | Memory: 47.7 MB (48.99%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,32 +1,19 @@
-
-
-class Solution {
-    public String getSignature(String s) {
-        int[] count = new int[26];
-        for (char c : s.toCharArray()) {
-            count[c - 'a']++;
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 26; i++) {
-            if (count[i] != 0) {
-                sb.append((char) ('a' + i)).append(count[i]);
+class Solution
+{
+    public List<List<String>> groupAnagrams(String[] strs)
+    {
+        Map<String,List<String>> map =new HashMap<>();
+        for(String s:strs)
+        {
+            char charArray[]=s.toCharArray();
+            Arrays.sort(charArray);
+            String sorted = new String(charArray);
+            if(!map.containsKey(sorted))
+            {
+                map.put(sorted,new LinkedList<String>());
             }
+            map.get(sorted).add(s);
         }
-        return sb.toString();
-    }
-
-    public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> result = new ArrayList<>();
-        Map<String, List<String>> groups = new HashMap<>();
-
-        for (String s : strs) {
-            groups.computeIfAbsent(getSignature(s), k -> new ArrayList<>()).add(s);
-        }
-
-        result.addAll(groups.values());
-
-        return result;
+        return new LinkedList<>(map.values());
     }
 }
-
