765ba0af44d6f8a960084171a17d5335e732deae
Prajwal Soni <143695149+prajwalsoni@users.noreply.github.com>
2024-05-13 12:39:48 +0530

    Time: 11 ms (92.72%) | Memory: 48.7 MB (34.68%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -2,40 +2,31 @@ import java.util.*;
 
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        List<Integer>[] bucket = new List[nums.length + 1];
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
+       List <Integer> [] bucket = new List[nums.length+1];
+       Map <Integer,Integer> frequencyMap = new HashMap<>();
 
-        for (int n : nums) {
-            frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
-        }
+       for(int n : nums) {
+          frequencyMap.put(n, frequencyMap.getOrDefault(n,0)+1);
+       } 
+       
+       for(int key : frequencyMap.keySet()) {
+          int frequency = frequencyMap.get(key);
+          if(bucket[frequency] == null) {
+              bucket[frequency] = new ArrayList<>();
+          }
+          bucket[frequency].add(key);
+       }
+       
+       int [] res = new int[k];
+       int counter = 0;
 
-        int maxFrequency = 0;
-        for (int frequency : frequencyMap.values()) {
-            maxFrequency = Math.max(maxFrequency, frequency);
-        }
-
-        for (int key : frequencyMap.keySet()) {
-            int frequency = frequencyMap.get(key);
-
-            if (bucket[frequency] == null) {
-                bucket[frequency] = new ArrayList<>();
-            }
-            bucket[frequency].add(key);
-        }
-
-        int[] res = new int[k];
-        int counter = 0;
-
-        for (int pos = bucket.length - 1; pos >= 0 && counter < k; pos--) {
-            if (bucket[pos] != null) {
-                for (Integer integer : bucket[pos]) {
-                    res[counter++] = integer;
-                    if (counter == k) {
-                        break;
-                    }
-                }
-            }
-        }
-        return res;
+       for(int pos = bucket.length-1; pos >= 0 && counter < k; pos--) {
+           if(bucket[pos] != null) {
+               for(Integer integer : bucket[pos]) {
+                   res[counter++] = integer;     
+               }
+           }
+       }
+       return res;
     }
 }
