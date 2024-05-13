552beb798594629d7b14cab07563c0e87607a2fc
Prajwal Soni <143695149+prajwalsoni@users.noreply.github.com>
2024-05-13 12:35:49 +0530

    Time: 11 ms (92.72%) | Memory: 48.3 MB (59.83%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -2,27 +2,40 @@ import java.util.*;
 
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        // Step 1: Count the frequency of each number
+        List<Integer>[] bucket = new List[nums.length + 1];
         Map<Integer, Integer> frequencyMap = new HashMap<>();
-        for (int num : nums) {
-            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+
+        for (int n : nums) {
+            frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
         }
-        
-        // Step 2: Find the top K frequent numbers using a min-heap
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>((n1, n2) -> frequencyMap.get(n1) - frequencyMap.get(n2));
-        for (int num : frequencyMap.keySet()) {
-            minHeap.add(num);
-            if (minHeap.size() > k) {
-                minHeap.poll();
+
+        int maxFrequency = 0;
+        for (int frequency : frequencyMap.values()) {
+            maxFrequency = Math.max(maxFrequency, frequency);
+        }
+
+        for (int key : frequencyMap.keySet()) {
+            int frequency = frequencyMap.get(key);
+
+            if (bucket[frequency] == null) {
+                bucket[frequency] = new ArrayList<>();
+            }
+            bucket[frequency].add(key);
+        }
+
+        int[] res = new int[k];
+        int counter = 0;
+
+        for (int pos = bucket.length - 1; pos >= 0 && counter < k; pos--) {
+            if (bucket[pos] != null) {
+                for (Integer integer : bucket[pos]) {
+                    res[counter++] = integer;
+                    if (counter == k) {
+                        break;
+                    }
+                }
             }
         }
-        
-        // Step 3: Store the top K frequent numbers in an array
-        int[] result = new int[k];
-        for (int i = k - 1; i >= 0; i--) {
-            result[i] = minHeap.poll();
-        }
-        
-        return result;
+        return res;
     }
 }
